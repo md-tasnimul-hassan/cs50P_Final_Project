c6b15bbb8f6317e0db17afef9f26bee516dacf4c
Md. Tasnimul Hassan <116421029+md-tasnimul-hassan@users.noreply.github.com>
2025-11-16 13:46:24 +0000

    updated

--- tick-tack-toe.py
+++ tick-tack-toe.py
@@ -1,24 +1,36 @@
+import re
 firtUserMove = True 
 
 def printBoard(board):
     print(f"  {board[0][0]}  |  {board[0][1]}  |  {board[0][2]}\n--------------\n  {board[1][0]}  |  {board[1][1]}  |  {board[1][2]}\n--------------\n  {board[2][0]}  |  {board[2][1]}  |  {board[2][2]}\n")
 
-def checkStatus():
+def checkStatus(board):
+    # it checks the current board status. If game ends it returns [True, winnerMarker], if game continues it returns [False, ""] and if game draws, it returns [True, "."]
     pass
 
 def userMove():
     if(firtUserMove):
-        x,y = input("Input in the following pattern => 1 2\nYour Move: ")
+        print("Input in the following pattern => 1 2")
         firtUserMove = False
-    else:
-        print("Your Move: ")
-    
+    while True:
+        try:
+            response = input("Your Move: ").strip()
+            matches = re.search ("^(\d{1}) (\d{1})$", response)
+            x = int(matches.group(1))
+            y = int(matches.group(2))
+            if(x not in [1, 2, 3] or y not in [1, 2, 3]):
+                raise ValueError
+            break
+        except:
+            print("Invalid input!")
+            pass
+    return [x-1,y-1]
     
 
 def aiMove():
     pass
 
-def main():
+def tickTacToe():
     board = [[""]*3 for _ in range(3)]
     for i in range(3):
         for j in range(3):
@@ -32,5 +44,4 @@ def main():
     printBoard(board)
 
 
-if(__name__ == "__main__"):
-    main()
+tickTacToe()
